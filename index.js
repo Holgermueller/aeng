f63026c8992dba52f296c779e9355c72177fd58a
holgermueller <9jholger@gmail.com>
2022-10-29 13:13:34 -0500

    can generate negative numbers

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ const generateNumber = () => {
   const hasLetters = document.getElementById("letters").value;
   const isNegative = document.getElementById("negative").value;
   let numbers = "0123456789";
+  const dash = "-";
   let alpha = "qwertyuiopasdfghjklzxcvbnm";
 
   let chars = numbers;
 
-  isNegative === "yes" ? (chars += "-") : "";
+  let randLetter = alpha.charAt(Math.floor(Math.random() * alpha.length));
 
   let earthNumber = "";
 
-  for (let i = 0; i < numLength; i++) {
-    earthNumber += chars.charAt(Math.floor(Math.random() * chars.length));
+  if (isNegative === "yes") {
+    earthNumber = earthNumber + dash;
+    for (let i = 0; i < numLength; i++) {
+      earthNumber += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+  } else {
+    for (let i = 0; i < numLength; i++) {
+      earthNumber += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
   }
 
-  console.log(isNegative, hasLetters);
+  console.log(randLetter);
 
   document.getElementById("numberDisplay").innerHTML = earthNumber;
 };
