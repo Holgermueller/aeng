9b13aabbbb5aad252565cf7cbac7a31977752fca
holgermueller <9jholger@gmail.com>
2022-10-29 10:15:38 -0500

    got form to cooperate

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const length = document.getElementById("length").value;
-const letters = document.getElementById("letters").value;
-const dashes = document.getElementById("dashes").value;
-const negative = document.getElementById("negative").value;
-
 const generateNumber = () => {
-  console.log(length, letters, dashes, negative);
+  const numLength = document.getElementById("numLength").value;
+  const letters = document.getElementById("letters").value;
+  const dashes = document.getElementById("dashes").value;
+  const negative = document.getElementById("negative").value;
+
+  console.log(numLength, letters, dashes, negative);
 };
