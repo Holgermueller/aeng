cb0ecade54aff77411e1335aa9c11177dc93712d
holgermueller <9jholger@gmail.com>
2023-11-18 14:23:35 -0600

    added about modal

--- index.js
+++ index.js
@@ -50,3 +50,17 @@ const resetForm = () => {
   document.getElementById("letters").selectedIndex = 1;
   document.getElementById("negative").selectedIndex = 1;
 };
+
+const modal = document.getElementById("myModal");
+
+const button = document.getElementById("modalButton");
+
+const closeButton = document.getElementById("span");
+
+const showModal = () => {
+  modal.style.display = "block";
+};
+
+const hideModal = () => {
+  modal.style.display = "none";
+};
